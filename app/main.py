d74410fc0b256cfba2ef1fb17f6e59bbea142ac7
Swanand Khalegaonkar <swanandkhalegaonkar@gmail.com>
2026-07-16 18:17:44 +0000

    feat: add system monitoring endpoint using psutil

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
+import platform
+import socket
+import psutil
 
 app = FastAPI(
     title="AI DevOps Monitoring Dashboard",
     version="1.0.0",
-    description="A FastAPI application for monitoring system metrics and AI-based analysis."
+    description="Monitor system metrics and analyze them using AI."
 )
 
 
 @app.get("/")
 def root():
     return {
-        "message": "Welcome to AI DevOps Monitoring Dashboard 🚀"
+        "message": "THIS IS MY NEW VERSION 🚀"
     }
 
 
@@ -18,4 +21,17 @@ def root():
 def health():
     return {
         "status": "healthy"
+    }
+
+
+@app.get("/system")
+def system_info():
+    return {
+        "hostname": socket.gethostname(),
+        "operating_system": platform.system(),
+        "os_version": platform.release(),
+        "processor": platform.processor(),
+        "cpu_percent": psutil.cpu_percent(interval=1),
+        "memory_percent": psutil.virtual_memory().percent,
+        "disk_percent": psutil.disk_usage('/').percent,
     }
